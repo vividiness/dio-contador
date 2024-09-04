d8a80e9f0b28ad988abdae8a99391b6ab487a9f7
Viviane Gomes <vivianegdev@gmail.com>
2024-09-04 19:02:58 -0300

    initial commit

---
+++ src/Contador.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Contador {
+
+  public static void main(String[] args) {
+
+    Scanner terminal = new Scanner(System.in);
+    System.out.println("Digite o primeiro parâmetro");
+    int parametroUm = terminal.nextInt();
+
+    System.out.println("Digite o segundo parâmetro");
+    int parametroDois = terminal.nextInt();
+
+    try {
+      contar(parametroUm, parametroDois);
+
+    } catch (ParametrosInvalidosException exception) {
+      System.out.println(exception.getMessage());
+    }
+  }
+
+    static void contar(int pUm, int pDois) throws ParametrosInvalidosException {
+
+      if(pUm > pDois){
+        throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+      }
+
+      int contagem = pDois - pUm;
+      for (int i = 1; i <= contagem; i++) {
+        System.out.println("Imprimindo o número " + i);
+      }
+
+  }
+}
